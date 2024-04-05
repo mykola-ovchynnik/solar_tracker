1e316144e1eeaf93f83c87b6a4e7015aa2151dbc
mykola-ovchynnik <ovchynnik.mykola@gmail.com>
2024-04-05 18:42:30 +0200

    enable pins 11 and 12 for interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@ void setup()
   pinMode(PIN_AXIS_BTN, INPUT);
   pinMode(PIN_POTENTIOMETR, INPUT);
 
+  // Enable pin change interrupt for PCINT2 (pins 8 to 13)
+  PCICR |= (1 << PCIE2);
+
+  // Enable interrupts for pins 11 (PCINT2) and 12 (PCINT3) within PCINT2
+  PCMSK2 |= (1 << PCINT2) | (1 << PCINT3);
+
   attachInterrupt(digitalPinToInterrupt(PIN_MODE_BTN), readModeBtn, RISING);
   attachInterrupt(digitalPinToInterrupt(PIN_AXIS_BTN), readAxisBtn, RISING);
 
